ebc39a4f8d528de6bb15ab94b8f9a48670e3ada4
Milen Khanal <milenkhanal@gmail.com>
2019-08-08 13:00:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,6 +6,12 @@
 #   end
 
  def my_own_map(array)
-  array.tap{|n|-1*n}
-  yield array
+  def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  yield new
 end
