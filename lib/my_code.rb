4a70e3938c7955cb97579b462d5f78713dec4c59
Milen Khanal <milenkhanal@gmail.com>
2019-08-08 12:40:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,6 +5,7 @@
 #     expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
 #   end
 
-def my_own_map (array)
-  yield { |b| "#{array.length *-1"}
-  end
+do |b| -1*b
+  arrray =[]
+  yield(array.length[] )
+end
