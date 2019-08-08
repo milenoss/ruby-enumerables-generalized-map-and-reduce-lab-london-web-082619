47903f376d1e169502307a562d8f949e9405aad7
Milen Khanal <milenkhanal@gmail.com>
2019-08-08 15:31:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+
+ def map (array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push( yield array[i])
+    i += 1
+  end
+  return new
+ end
+ 
+ def reduce(array, total = 0)
+ i= 0
+ while i< array.length do 
+   total = ( yield total, array[i])
+   i += 1
+ end
+ if total == 0 
+   return true
+ end
+ return total
+ end
+
